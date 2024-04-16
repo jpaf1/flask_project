c0762992a22ce2900a066e18e86acc5fe145cc62
jpaf1 <kirill121204@gmail.com>
2024-04-16 14:38:17 +0300

    create class User and route users/create

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+USERS = [] #list for objects of type user
+
 from app import views
+from app import models
--- app/views.py
+++ app/views.py
@@ -1,5 +1,35 @@
 from app import app
+from flask import request, Response
+from app import USERS, models
+import json
 
 @app.route('/')
 def index():
     return "<h1>Hello world<h1>"
+
+
+@app.post('/users/create', )
+def user_create():
+    data = request.get_json()
+    id = len(USERS)
+    first_name = data["first_name"]
+    last_name = data["last_name"]
+    email = data["email"]
+
+    # todo check email
+
+    user = models.User(id, first_name, last_name, email)
+    USERS.append(user)
+    response = Response(
+        json.dumps({
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email,
+            "total_reactions": user.total_reactions,
+            "posts": user.posts,
+        }),
+        200,
+        mimetype = "application/json",
+    )
+    return response
